3b0b1b85aa7c05c14e65bc85be56e0ac7b4f012e
Scott Davis <skawtus@gmail.com>
2019-09-02 14:12:05 -0700

    new exifremove & modules

--- go.mod
+++ go.mod
@@ -4,13 +4,11 @@ go 1.12
 
 require (
 	github.com/blang/semver v3.5.1+incompatible // indirect
-	github.com/dsoprea/go-exif v0.0.0-20190421204329-061c6b48d883
+	github.com/dsoprea/go-exif v0.0.0-20190901173045-3ce78807c90f
 	github.com/dsoprea/go-jpeg-image-structure v0.0.0-20190422055009-d6f9ba25cf48
-	github.com/dsoprea/go-logging v0.0.0-20190409182557-13b4fff49234
+	github.com/dsoprea/go-logging v0.0.0-20190624164917-c4f10aab7696
 	github.com/dsoprea/go-png-image-structure v0.0.0-20190624104353-c9b28dcdc5c8
 	github.com/dyatlov/go-opengraph v0.0.0-20180429202543-816b6608b3c8 // indirect
-	github.com/go-errors/errors v1.0.1 // indirect
-	github.com/golang/geo v0.0.0-20190507233405-a0e886e97a51 // indirect
 	github.com/golang/protobuf v1.3.1 // indirect
 	github.com/google/uuid v1.1.1 // indirect
 	github.com/gorilla/websocket v1.4.0 // indirect
@@ -24,21 +22,17 @@ require (
 	github.com/pborman/uuid v1.2.0 // indirect
 	github.com/pelletier/go-toml v1.3.0 // indirect
 	github.com/pkg/errors v0.8.1
+	github.com/scottleedavis/go-exif-remove v0.0.0-20190902210608-b25a47538306 // indirect
 	github.com/spf13/afero v1.2.2 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
-	github.com/stretchr/testify v1.3.0
+	github.com/stretchr/testify v1.4.0
 	go.uber.org/atomic v1.3.2 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.9.1 // indirect
-	golang.org/x/crypto v0.0.0-20190404164418-38d8ce5564a5 // indirect
 	golang.org/x/image v0.0.0-20190703141733-d6a02ce849c9
-	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3 // indirect
-	golang.org/x/sys v0.0.0-20190405154228-4b34438f7a67 // indirect
-	golang.org/x/text v0.3.1-0.20180807135948-17ff2d5776d2 // indirect
 	google.golang.org/appengine v1.4.0 // indirect
 	google.golang.org/genproto v0.0.0-20190404172233-64821d5d2107 // indirect
 	google.golang.org/grpc v1.20.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
 
--- server/plugin.go
+++ server/plugin.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	"bytes"
-	"errors"
-	"fmt"
 	"image"
 	"io"
 	"io/ioutil"
 	"strings"
 	"sync"
 
-	"github.com/dsoprea/go-exif"
-	"github.com/dsoprea/go-jpeg-image-structure"
-	"github.com/dsoprea/go-png-image-structure"
+	"github.com/scottleedavis/go-exif-remove"
 	"github.com/mattermost/mattermost-server/model"
 	"github.com/mattermost/mattermost-server/plugin"
 )
@@ -25,223 +21,28 @@ type Plugin struct {
 	configuration *configuration
 }
 
-const (
-	JpegMediaType  = "jpeg"
-	PngMediaType   = "png"
-	OtherMediaType = "other"
-	StartBytes    = 4
-	EndBytes      = 4
-	OffsetBytes   = 4
-)
-
-type MediaContext struct {
-	MediaType string
-	RootIfd   *exif.Ifd
-	RawExif   []byte
-	Media     interface{}
-}
-
-type IfdEntry struct {
-	IfdPath     string      `json:"ifd_path"`
-	FqIfdPath   string      `json:"fq_ifd_path"`
-	IfdIndex    int         `json:"ifd_index"`
-	TagId       uint16      `json:"tag_id"`
-	TagName     string      `json:"tag_name"`
-	TagTypeId   uint16      `json:"tag_type_id"`
-	TagTypeName string      `json:"tag_type_name"`
-	UnitCount   uint32      `json:"unit_count"`
-	Value       interface{} `json:"value"`
-	ValueString string      `json:"value_string"`
-}
-
 func (p *Plugin) FileWillBeUploaded(c *plugin.Context, info *model.FileInfo, file io.Reader, output io.Writer) (*model.FileInfo, string) {
 
 	switch strings.ToUpper(info.Extension) {
 	case "JPG", "JPEG", "PNG":
 		if data, err := ioutil.ReadAll(file); err != nil {
 			p.API.LogError(err.Error())
-			return nil, ""
+			return nil, err.Error()
 		} else {
-			_, _, err = image.Decode(bytes.NewReader(data))
-			if err != nil {
-				p.API.LogInfo("ERROR: original image is corrupt" + err.Error() + "\n")
-				return nil, ""
+			if _, _, err = image.Decode(bytes.NewReader(data)); err != nil {
+				errMsg := "ERROR: original image is corrupt " + err.Error()
+				p.API.LogInfo(errMsg)
+				return nil, errMsg
 			}
-			mc, entries := p.parseEXIF(data)
-			if len(entries) == 0 {
-				return nil, ""
-			}
-
-			filteredBytes := []byte{}
-
-			switch mc.MediaType {
-			case JpegMediaType:
-				if filteredBytes, err = p.extractJPEGEXIF(mc, data, filteredBytes); err != nil {
-					p.API.LogError(err.Error())
-					return nil, ""
-				}
-			}
-
-			if _, err := output.Write(filteredBytes); err != nil {
+			if filtered, err := exifremove.Remove(data); err != nil {
 				p.API.LogError(err.Error())
+				return nil, err.Error()
+			} else {
+				if _, err := output.Write(filtered); err != nil {
+					p.API.LogError(err.Error())
+				}
 			}
 		}
 	}
 	return nil, ""
-}
-
-func (p *Plugin) extractJPEGEXIF(mc *MediaContext, data []byte, filtered []byte) ([]byte, error) {
-	sl := mc.Media.(*jpegstructure.SegmentList)
-	_, sExif, err := sl.FindExif()
-	if err != nil {
-		return nil, errors.New("No EXIF in image")
-	}
-
-	p.API.LogInfo(fmt.Sprintf("****(exif) %x %s %x", sExif.Offset, sExif.MarkerName, len(sExif.Data)))
-
-	bytesCount := 0
-	startExifBytes := StartBytes
-	endExifBytes := EndBytes
-	for _, s := range sl.Segments() {
-
-		if s.MarkerName == sExif.MarkerName {
-			if startExifBytes == StartBytes {
-				startExifBytes = bytesCount
-				endExifBytes = startExifBytes + len(s.Data) + OffsetBytes
-			} else {
-				endExifBytes += len(s.Data) + OffsetBytes
-			}
-		}
-		bytesCount += len(s.Data) + OffsetBytes
-
-		p.API.LogInfo(fmt.Sprintf("%x %s %v (%x)", s.Offset, s.MarkerName, len(s.Data), s.Offset+len(s.Data)))
-
-	}
-
-	filtered = data[:startExifBytes]
-	filtered = append(filtered, data[endExifBytes:]...)
-
-	p.API.LogInfo(fmt.Sprintf("********(size) %v %v  (%v)", len(data), len(filtered), len(data)-len(filtered)))
-
-	//TODO enable when > 95% effective EXIF without corruption
-	//_, _, err = image.Decode(bytes.NewReader(filtered))
-	//if err != nil {
-	//	return nil, errors.New("EXIF extraction corrupted image " + err.Error())
-	//}
-
-	return filtered, nil
-}
-
-func (p *Plugin) parseEXIF(data []byte) (mc *MediaContext, entries []IfdEntry) {
-
-	jmp := jpegstructure.NewJpegMediaParser()
-	pmp := pngstructure.NewPngMediaParser()
-	mc = &MediaContext{
-		MediaType: OtherMediaType,
-		RootIfd:   nil,
-		RawExif:   nil,
-		Media:     nil,
-	}
-
-	if jmp.LooksLikeFormat(data) {
-		mc.MediaType = JpegMediaType
-	} else if pmp.LooksLikeFormat(data) {
-		mc.MediaType = PngMediaType
-	}
-
-	switch mc.MediaType {
-	case JpegMediaType:
-		sl, err := jmp.ParseBytes(data)
-		if err != nil {
-			return mc, []IfdEntry{}
-		}
-
-		mc.Media = sl
-
-		rootIfd, rawExif, err := sl.Exif()
-		if err != nil {
-			return mc, []IfdEntry{}
-		}
-
-		mc.RootIfd = rootIfd
-		mc.RawExif = rawExif
-
-	case PngMediaType:
-		cs, err := pmp.ParseBytes(data)
-		if err != nil {
-			return mc, []IfdEntry{}
-		}
-
-		mc.Media = cs
-
-		rootIfd, rawExif, err := cs.Exif()
-		if err != nil {
-			return mc, []IfdEntry{}
-		}
-
-		mc.RootIfd = rootIfd
-		mc.RawExif = rawExif
-	default:
-		return mc, []IfdEntry{}
-	}
-
-	im := exif.NewIfdMappingWithStandard()
-	ti := exif.NewTagIndex()
-
-	entries = make([]IfdEntry, 0)
-	visitor := func(fqIfdPath string, ifdIndex int, tagId uint16, tagType exif.TagType, valueContext exif.ValueContext) (err error) {
-		defer func() {
-			if state := recover(); state != nil {
-				p.API.LogError(state.(error).Error())
-			}
-		}()
-
-		ifdPath, err := im.StripPathPhraseIndices(fqIfdPath)
-		if err != nil {
-			return err
-		}
-
-		it, err := ti.Get(ifdPath, tagId)
-		if err != nil {
-			return err
-		}
-
-		valueString := ""
-		var value interface{}
-		if tagType.Type() == exif.TypeUndefined {
-			value, err = exif.UndefinedValue(ifdPath, tagId, valueContext, tagType.ByteOrder())
-			if err != nil {
-				return err
-			} else {
-				valueString = fmt.Sprintf("%v", value)
-			}
-		} else {
-			valueString, err = tagType.ResolveAsString(valueContext, true)
-			if err != nil {
-				return err
-			}
-			value = valueString
-		}
-
-		entry := IfdEntry{
-			IfdPath:     ifdPath,
-			FqIfdPath:   fqIfdPath,
-			IfdIndex:    ifdIndex,
-			TagId:       tagId,
-			TagName:     it.Name,
-			TagTypeId:   tagType.Type(),
-			TagTypeName: tagType.Name(),
-			UnitCount:   valueContext.UnitCount,
-			Value:       value,
-			ValueString: valueString,
-		}
-
-		entries = append(entries, entry)
-
-		return nil
-	}
-
-	exif.Visit(exif.IfdStandard, im, ti, mc.RawExif, visitor)
-
-	return mc, entries
 }
